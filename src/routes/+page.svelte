7f29d98b0b8a94de15b0e3df4d5d70c0a4634ae2
Kyle McDonald <kyle@kylemcdonald.net>
2024-03-06 23:31:33 -0800

    working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import projectsData from './projects.json';
+
+    let { contact, biography, works } = projectsData;
+
+    works = works.sort((a, b) => b.year - a.year);
+</script>
+
+<main>
+	<h1>Kyle McDonald</h1>
+
+	<div>
+		<a href="https://twitter.com/{contact.twitter}" class="contact" style="float:left"
+			>@{contact.twitter}</a
+		>
+		<a href="mailto:{contact.email}" class="contact" style="float:right">{contact.email}</a>
+	</div>
+
+	<br style="clear:both" />
+
+	<div class="section">
+		<div class="cell project">
+			<h2>Biography</h2>
+			<div id="bio">
+				{@html biography}
+			</div>
+		</div>
+
+		{#each works as work, i (work)}
+			<div class="cell project">
+                {#if i == 0}
+                    <h2>Projects</h2>
+                {/if}
+				<a href={work.links[0].url} name={work.name} target="_blank">
+					<img src={`/thumbs/${work.image}`} alt={work.name} />
+				</a>
+				<div class="description">
+					<h3>
+						{work.name}
+						<em
+							>{work.year}
+							{#if work.client}
+                                for <a href={work.client.url}>{work.client.name}</a>
+							{/if}
+							{#each work.collaborators as collaborator, j (collaborator)}
+								{#if j === 0}
+									with
+								{:else}
+									&nbsp;and
+								{/if}
+								<a href={collaborator.url}>{collaborator.name}</a>
+							{/each}
+						</em>
+					</h3>
+                    {@html work.description}
+				</div>
+				<ul>
+					{#each work.links as link}
+						<li><a href={link.url}>{link.text}</a></li>
+					{/each}
+				</ul>
+			</div>
+		{/each}
+	</div>
+</main>
+
+<style>
+	@import url('style.css');
+</style>
